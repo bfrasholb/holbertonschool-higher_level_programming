fc9bd46b8de1071f85051c8c205826ec274bcf3d
bfrasholb <13065@holbertonstudents.com>
2026-05-09 00:50:47 +1000

    last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import random
+number = random.randint(-10000, 10000)
+
+if number > 0:
+    remainder = number % 10
+else:
+    remainder = ( -1 * number ) % 10
+
+if remainder > 5:
+    compare = "and is greater than 5"
+elif remainder == 0:
+    compare = "and is 0"
+elif remainder < 6:
+    compare = "and is less than 6 and not 0"
+
+print(f"Last digit of {number} is {remainder} {compare}")
