b24bd38ed26134bd0d1917c01e01d124ae2704ff
bfrasholb <13065@holbertonstudents.com>
2026-05-10 01:12:37 +1000

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     if argc == 1:
         print("0 arguments.")
     elif argc == 2:
-        print("1 argument.")
+        print("1 argument:")
     for i in range(1, argc):
         print("{}: {}".format(i, argv[i]))
