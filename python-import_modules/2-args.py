ed551600cf5dacc20ce0c12808e77efe28aa2925
bfrasholb <13065@holbertonstudents.com>
2026-05-10 01:11:53 +1000

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,10 +4,9 @@ from sys import argv
 
 if __name__ == "__main__":
     argc = len(argv)
-    if argc == 0:
+    if argc == 1:
         print("0 arguments.")
-    elif argc == 1:
+    elif argc == 2:
         print("1 argument.")
-    elif argc > 1:
-        for i in range(1, argc):
-            print("{}: {}".format(i, argv[i]))
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
