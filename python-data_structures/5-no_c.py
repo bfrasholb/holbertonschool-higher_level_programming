d038acba81fd1bc0ca096f5ca1b29b15196ab603
bfrasholb <13065@holbertonstudents.com>
2026-05-12 14:55:39 +1000

    No C

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -6,4 +6,4 @@ def no_c(my_string):
     for i in range(0, len(my_string)):
         if my_string[i] != "c" and my_string[i] != "C":
             new_str += my_string[i]
-    print("{}".format(new_str, end=""))
+    print("{}".format(new_str))
