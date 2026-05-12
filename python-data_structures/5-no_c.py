e19cd55a61b25e2b4e64ac5c18e4b2f7cf73efbc
bfrasholb <13065@holbertonstudents.com>
2026-05-12 21:56:42 +1000

    lisp syntax

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
 
+# def no_c(my_string):
+#    new_str = ""
+#    for i in range(0, len(my_string)):
+#        if my_string[i] != "c" and my_string[i] != "C":
+#            new_str += my_string[i]
+#    return new_str
+
+
 def no_c(my_string):
-    new_str = ""
-    for i in range(0, len(my_string)):
-        if my_string[i] != "c" and my_string[i] != "C":
-            new_str += my_string[i]
-    return new_str
+    return "".join(
+        [
+            "" if my_string[i] == "c" or my_string[i] == "C" else my_string[i]
+            for i in range(0, len(my_string))
+        ]
+    )
