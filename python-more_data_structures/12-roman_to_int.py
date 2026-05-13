65c123e942d98bd72d8309d1852fb1f3b0984f51
bfrasholb <13065@holbertonstudents.com>
2026-05-13 12:46:59 +1000

    No more illegals in my dictionary

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
 
+def detect_illegal(roman_string):
+    banned_seq = ["VV", "LL", "DD", "IIII", "XXXX", "CCCC"]
+    for string in banned_seq:
+        if roman_string.find(string) != -1:
+            return True
+    return False
 
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not roman_string or \
+       not isinstance(roman_string, str) or \
+       detect_illegal(roman_string) is True:
         return 0
-    roman_values = {"I": 1, "V": 5, "X": 10, "L": 50,
-                    "C": 100, "D": 500, "M": 1000}
-
-    total = 0
-    prev = 0
+    roman_values = {"I": 1,
+                    "V": 5,
+                    "X": 10,
+                    "L": 50,
+                    "C": 100,
+                    "D": 500,
+                    "M": 1000}
+    total, prev_sym, sub = 0, "", False
 
     for char in reversed(roman_string):
         value = roman_values[char]
 
-        if value < prev:
+        if prev_sym != "" and value < roman_values[prev_sym]:
+            if sub:
+                return 0
             total -= value
+            sub = True
         else:
             total += value
-        prev = value
+            sub = False
+        prev_sym = char
     return total
