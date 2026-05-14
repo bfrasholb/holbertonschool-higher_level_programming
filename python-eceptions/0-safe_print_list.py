a16202b6dd1bf105c66273c4f5a5cd03953ed8d8
bfrasholb <13065@holbertonstudents.com>
2026-05-14 11:41:59 +1000

    Exception Handling

---
+++ python-eceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for item in my_list:
+        try:
+            if count < x:
+                print("{}".format(item), end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
