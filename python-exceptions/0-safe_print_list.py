0bea1cdf35e6d79433b10848fe770d2caf20d8d2
bfrasholb <13065@holbertonstudents.com>
2026-05-14 11:57:15 +1000

    To War!

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for item in my_list:
+    for item in range(x):
         try:
-            if count < x:
-                print("{}".format(item), end="")
+            print("{}".format(my_list[item]), end="")
             count += 1
         except IndexError:
             break
+    print()
     return count
