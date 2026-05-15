468b8abd8cbf7223391af1e923463c99309ec3f1
bfrasholb <13065@holbertonstudents.com>
2026-05-15 14:53:29 +1000

    Add integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    res = 0
+    try:
+        res = int(a) + int(b)
+    except Exception:
+        if type(a) not in (int, float):
+            res = "a must be an integer"
+        elif type(b) not in (int, float):
+            res = "b must be an integer"
+    return res
