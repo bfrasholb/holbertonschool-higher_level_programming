a77b9a61ba4e2734a8afb1867346cf9181a20525
bfrasholb <13065@holbertonstudents.com>
2026-05-17 12:15:38 +1000

    Indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def text_indentation(text):
+    """Prints text with 2 new lines after '.', '?' and ':'"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    length = len(text)
+
+    while i < length:
+        print(text[i], end="")
+
+        if text[i] in ".?:":
+            print("\n")
+
+            # skip spaces after punctuation
+            i += 1
+            while i < length and text[i] == " ":
+                i += 1
+            continue
+
+        i += 1
