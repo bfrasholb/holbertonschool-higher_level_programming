2f8031b11187d2eae5d2daf78d730ea77c8527cc
bfrasholb <13065@holbertonstudents.com>
2026-05-19 11:46:19 +1000

    Setting Size

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,10 +14,11 @@ class Square:
     @property
     def size(self):
         return self.__size
+
     @size.setter
-        def size(self, value):
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
