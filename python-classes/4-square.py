8ef0388c8ce2411c299adecca05e42e2e40d192d
bfrasholb <13065@holbertonstudents.com>
2026-05-19 11:41:58 +1000

    Setting Size

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,7 +6,7 @@ class Square:
     """The Square Class"""
 
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     def area(self):
         return self.__size**2
