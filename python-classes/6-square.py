97cf9c672f1972ca78a3598f5c40f95cc3d468ca
bfrasholb <13065@holbertonstudents.com>
2026-05-19 12:55:30 +1000

    Squary

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,17 +13,13 @@ class Square:
         return self.__size**2
 
     def my_print(self):
-        for i in range(self.position[1]):
-            print()
         if self.size == 0:
             print()
             return
+        for i in range(self.position[1]):
+            print()
         for i in range(self.size):
             print(" " * self.position[0] + "#" * self.size)
-
-    @property
-    def size(self):
-        return self.__size
 
     @property
     def position(self):
