293cbfa4eb8d863b82bedeccbdf6f4e65a5afdfc
bfrasholb <13065@holbertonstudents.com>
2026-05-26 17:56:30 +1000

    Interesting

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A Square Module"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """The Square Class"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {size}/{size}"
