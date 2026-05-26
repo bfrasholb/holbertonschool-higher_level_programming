cebbaef9c1500e2ad89793162c4371b6e1aa5d58
bfrasholb <13065@holbertonstudents.com>
2026-05-26 17:09:39 +1000

    Lolbies

---
+++ pld/lolbies/main_lolbies.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+Map = __import__("map").Map
+Summoner = __import__("summoner").Summoner
+
+lucian = Summoner("Lucian", 800, 500)
+warwick = Summoner("Warwick", 740, 550)
+nami = Summoner("Nami", 830, 460)
+velkoz = Summoner("Vel'koz", 620, 420)
+caitlin = Summoner("Caitlin", 580, 380)
+garen = Summoner("Garen", 640, 0)
+renekton = Summoner("Renekton", 620, 0)
+bard = Summoner("Bard", 600, 400)
+shaco = Summoner("Shaco", 580, 440)
+lulu = Summoner("Lulu", 580, 380)
+
+blue_team = [lucian, warwick, nami, velkoz, caitlin]
+red_team = [garen, renekton, bard, shaco, lulu]
+
+
+ranked = Map("Summoner's Rift", blue_team, red_team)
+
+print(ranked)
