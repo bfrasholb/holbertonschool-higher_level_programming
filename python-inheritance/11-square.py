84c82b9724c69347b3dcca5dc2b210ba3bbc7396
bfrasholb <13065@holbertonstudents.com>
2026-05-26 17:59:46 +1000

    Almost got me

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -10,6 +10,3 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
-
-    def __str__(self):
-        return f"[Square] {size}/{size}"
