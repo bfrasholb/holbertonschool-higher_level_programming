941483c3e314d6f4a2adf10a9f406b09903990c2
bfrasholb <13065@holbertonstudents.com>
2026-05-27 15:54:08 +1000

    len() my old pal

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -5,8 +5,11 @@ class CountedIterator:
     def __init__(self, iterable):
         self.__iterable = iter(iterable)
         self.__count = 0
+        self.__len = len(iterable)
 
     def __next__(self):
+        if self.__count == self.__len:
+            raise StopIteration()
         self.__count += 1
         return self.__iterable.__next__()
 
