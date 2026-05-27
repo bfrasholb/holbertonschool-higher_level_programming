25a286bb22349600e378c13953974d9296aa188f
bfrasholb <13065@holbertonstudents.com>
2026-05-27 15:35:54 +1000

    Override mode enabled

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -3,13 +3,17 @@
 
 class VerboseList(list):
     def append(self, element):
-        print(f"Added {element} to the list.")
+        print(f"Added [{element}] to the list.")
+        super().append(element)
 
-    def extend(self):
-        print(f"Extended the list with ")
+    def extend(self, items):
+        print(f"Extended the list with [{len(items)}].")
+        super().extend(items)
 
-    def remove(self):
-        pass
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
 
-    def pop(self):
-        pass
+    def pop(self, index=-1):
+        print(f"Popped [{self[index]}] from the list.")
+        super().pop(index)
