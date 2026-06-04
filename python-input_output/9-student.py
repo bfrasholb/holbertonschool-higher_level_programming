cd0047fd4d6295f24c06a94aaa837f51583ee832
bfrasholb <13065@holbertonstudents.com>
2026-06-04 17:53:52 +1000

    removed getters + setter

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,29 +10,5 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    @property
-    def age(self):
-        return self.__age
-
-    @age.setter
-    def age(self, age):
-        self.__age = age
-
-    @property
-    def first_name(self):
-        return self.__first_name
-
-    @first_name.setter
-    def first_name(self, first_name):
-        self.__first_name = first_name
-
-    @property
-    def last_name(self):
-        return self.__last_name
-
-    @last_name.setter
-    def last_name(self, last_name):
-        self.__last_name = last_name
-
     def to_json(self):
         return self.__dict__
