c996016a549564a6f074c5c414c51b040c7ac369
bfrasholb <13065@holbertonstudents.com>
2026-06-04 17:16:28 +1000

    Adding Items

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Add Elements"""
 
-import json
 from sys import argv
 
 to_json = __import__("5-save_to_json_file").save_to_json_file
 from_json = __import__("6-load_from_json_file").load_from_json_file
-
-
-to_json(argv[1:], "add_item")
+stored_data = "add_item.json"
+try:
+    my_list = from_json(stored_data)
+    my_list += argv[1:]
+except:
+    my_list = argv[1:]
+to_json(my_list, stored_data)
