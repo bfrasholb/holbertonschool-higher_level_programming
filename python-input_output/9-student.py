0d349685eb3aee52085bde2c98f0ba6ea680132b
bfrasholb <13065@holbertonstudents.com>
2026-06-04 17:40:59 +1000

    Student Properties

---
+++ python-input_output/9-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        self.__first_name = first_name
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        self.__last_name = last_name
+
+    def to_json(self):
+        return self.__dict__
