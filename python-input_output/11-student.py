2353810c3303df0749b08cdc3fd238c61d97023f
bfrasholb <13065@holbertonstudents.com>
2026-06-05 23:58:44 +1000

    comprehensive lists

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -19,6 +19,4 @@ class Student:
             return filtered_dict
 
     def reload_from_json(self, json):
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        [setattr(self, key, value) for key, value in json.items()]
