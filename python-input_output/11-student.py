e17bc60f5324bfab4c6d729d82656ab8f6f4fa06
bfrasholb <13065@holbertonstudents.com>
2026-06-05 23:42:16 +1000

    reload from file

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs: list = ["age", "last_name", "first_name"]):
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            filtered_dict = {
+                key: value for key, value
+                in self.__dict__.items() if key in attrs
+            }
+            return filtered_dict
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
