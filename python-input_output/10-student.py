de21dc813bfe670a7828079face68c1d2efc588a
bfrasholb <13065@holbertonstudents.com>
2026-06-05 23:05:24 +1000

    Retrieving keys

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+import json
+
+
+class Student:
+    """Student Class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs: list = ["age", "last_name", "first_name"]):
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in sorted(attrs)):
+            filtered_dict = {
+                key: value for key, value in self.__dict__.items() if key in attrs
+            }
+            return json.dumps(filtered_dict)
