97c4126ecd3dc1549c184e68129d7e7b411ff339
bfrasholb <13065@holbertonstudents.com>
2026-06-07 19:13:07 +1000

    Xmlent

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""XML"""
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """
+    Takes Python dict + filename
+    serialize dict to XML
+    save to filename
+    """
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+    tree = Et.ElementTree(root)
+    tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    """
+    Takes filename as its parameter
+    Reads the XML data from file
+    Return deserialized python dict
+    """
+    tree = ET.parse(filename)
+    result = {}
+    for child in tree:
+        result[child.tag] = child.text
+    return result
