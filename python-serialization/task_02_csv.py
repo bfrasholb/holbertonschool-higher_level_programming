dac61fb05ef083bdad6a9aced61ee31b9826ee0b
bfrasholb <13065@holbertonstudents.com>
2026-06-07 18:56:09 +1000

    its always a list

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -15,12 +15,10 @@ def convert_csv_to_json(filename: str):
     """
     try:
         with open(filename, "r", newline="") as f:
-            reader = csv.reader(f)
-            data_json = []
-            for row in reader:
-                data_json.append([row for row in reader])
-        with open("data.json", "w", data_json) as f:
-            f.dump(data_json, f)
+            data_json = list(csv.DictReader(f))
+        with open("data.json", "w") as f:
+            json.dump(data_json, f)
         return True
-    except Exception:
+    except Exception as err:
+        print(err)
         return False
