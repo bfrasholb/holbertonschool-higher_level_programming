cef18093125676767757501e3dd4f608f057e457
bfrasholb <13065@holbertonstudents.com>
2026-06-15 12:53:43 +1000

    Formatting

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import requests
 import csv
 
@@ -12,7 +10,6 @@ def fetch_and_print_posts():
 
     if response.status_code == 200:
         data = response.json()
-
         for post in data:
             print(post.get("title"))
 
@@ -22,17 +19,10 @@ def fetch_and_save_posts():
 
     if response.status_code == 200:
         data = response.json()
+        posts_list = [{"id": post.get("id"), "title": post.get(
+            "title"), "body": post.get("body")} for post in data]
 
-        posts_list = [
-            {
-                "id": post.get("id"),
-                "title": post.get("title"),
-                "body": post.get("body"),
-            }
-            for post in data
-        ]
-
-        with open("posts.csv", mode="w", newline="", encoding="utf-8") as file:
+        with open("posts.csv", mode="w", newline="", encoding="utf-8") as f:
             writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
             writer.writeheader()
             writer.writerows(posts_list)
