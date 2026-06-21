779e2b4be01f48a090e7eb4a19d07fe0edd5824a
bfrasholb <13065@holbertonstudents.com>
2026-06-21 16:03:34 +1000

    States

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Creates a Database and a Table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-	id INT UNIQUE NOT NULL PRIMARY KEY,
+	id INT PRIMARY KEY,
 	name VARCHAR(256)
 );
