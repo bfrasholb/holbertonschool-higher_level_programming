b9f5e59fa95547790ef1982f311c79b2df43551d
bfrasholb <13065@holbertonstudents.com>
2026-06-21 16:43:24 +1000

    I dont know whats wrong

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,6 @@
 -- Lists All Cities In Database
 SELECT cities.name, states.name
-FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+FROM cities
+JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
