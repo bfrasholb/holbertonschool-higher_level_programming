291b65384bcf02203863da47c14cac963215c9e7
bfrasholb <13065@holbertonstudents.com>
2026-06-21 16:41:17 +1000

    Nevermind i see it

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Lists All Cities In Database
 SELECT cities.name, states.name
-FROM cities JOIN states ON cities.state_id = state_id ORDER BY cities.id ASC;
+FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
