04a5c06bd1aca7345e0ec359476d3cc012ee401c
bfrasholb <13065@holbertonstudents.com>
2026-06-23 21:35:51 +1000

    This is pretty nice

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Search states by name'''
+
+import MySQLdb
+from sys import argv
+
+
+def search_states(username: str, password: str, database, state: str):
+    '''Function to search States'''
+    db = MySQLdb.connect(host='127.0.0.1', port=3306,
+                         user=username, passwd=password, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == state:
+            print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    search_states(argv[1], argv[2], argv[3], argv[4])
