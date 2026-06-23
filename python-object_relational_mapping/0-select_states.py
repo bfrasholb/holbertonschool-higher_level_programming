48199fd69ab75f325883f589eaf153d9cc4b3f7b
bfrasholb <13065@holbertonstudents.com>
2026-06-23 21:26:26 +1000

    Comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+'''Module Selects States From Table'''
 
 import MySQLdb
 from sys import argv
 
 
 def select_states(username: str, password: str, database):
+    '''Function to get States'''
     db = MySQLdb.connect(host='127.0.0.1', port=3306,
                          user=username, passwd=password, db=database)
     cur = db.cursor()
@@ -16,5 +18,5 @@ def select_states(username: str, password: str, database):
     db.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     select_states(argv[1], argv[2], argv[3])
