bfe8164920eef8d93f8c3cc4983afe1af112ee79
bfrasholb <13065@holbertonstudents.com>
2026-06-26 16:12:47 +1000

    My bad

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,8 @@ def filter_states(username, password, database, state):
                          user=username, passwd=password, db=database)
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE BINARY name ='{}' ORDER BY id ASC".format(state))
+    "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC",
+    (state,))
     rows = cur.fetchall()
     for row in rows:
         print(row)
