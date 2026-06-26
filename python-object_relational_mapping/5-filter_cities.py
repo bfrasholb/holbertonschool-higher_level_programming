26065ca0e9b70b051d7f20ec5ceacfaa8c112440
bfrasholb <13065@holbertonstudents.com>
2026-06-26 16:24:49 +1000

    Checking something

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,39 +1,33 @@
 #!/usr/bin/python3
-"""Lists all cities of a given state"""
+"""Lists all cities from the database."""
 
 import MySQLdb
-from sys import argv
-
-
-def filter_cities(username, password, database, state_name):
-    """Function that lists cities by state name safely"""
-    db = MySQLdb.connect(
-        host="127.0.0.1",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    cur = db.cursor()
-
-    cur.execute("""
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """, (state_name,))
-
-    rows = cur.fetchall()
-
-    if rows:
-        print(", ".join(city[0] for city in rows))
-
-    cur.close()
-    db.close()
+import sys
 
 
 if __name__ == "__main__":
-    if len(argv) == 5:
-        filter_cities(argv[1], argv[2], argv[3], argv[4])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "INNER JOIN states "
+        "ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
