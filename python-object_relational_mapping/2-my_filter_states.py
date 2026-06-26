8016be946b55043aae3d089ca743555347989ebe
bfrasholb <13065@holbertonstudents.com>
2026-06-26 16:10:12 +1000

    Parametised

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-"""Filter states by name"""
-
+"""Safe filter states by name (SQL injection safe)"""
 
 import MySQLdb
 from sys import argv
 
 
 def filter_states(username, password, database, state):
-    """Function to filter States"""
-    db = MySQLdb.connect(host='127.0.0.1', port=3306,
-                         user=username, passwd=password, db=database)
+    """Fetch states safely using parameterized query"""
+    db = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
     cur = db.cursor()
+
     cur.execute(
-        "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC", (state,))
+        "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC",
+        (state,)
+    )
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    filter_states(argv[1], argv[2], argv[3], argv[4])
+    if len(argv) == 5:
+        filter_states(argv[1], argv[2], argv[3], argv[4])
