f1ceeb564f8ced47bca044a3b710e7fe550f5086
bfrasholb <13065@holbertonstudents.com>
2026-07-09 17:44:58 +1000

    Fuck semistandard oml

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-args = process.argv.slice(2);
+const args = process.argv.slice(2);
 if (args[0]) console.log(args[0]);
 else if (args[0] === undefined) console.log('No argument');
