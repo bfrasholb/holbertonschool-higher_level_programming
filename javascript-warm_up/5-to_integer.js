dfbcea1829d15ba2a7d5dbc276bec325d99d004a
bfrasholb <13065@holbertonstudents.com>
2026-07-09 18:04:05 +1000

    I hate JavaScript

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-my_number = parseInt(args[0], 10)
-if (my_number) console.log('My number:', my_number)
-else console.log('Not a number')
+const myNumber = parseInt(args[0], 10);
+if (myNumber) console.log('My number:', myNumber);
+else console.log('Not a number');
