3524ed0bb580b04f06cc70482852d7417d832526
bfrasholb <13065@holbertonstudents.com>
2026-07-09 22:27:11 +1000

    Horrendousificly disgusting

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+let i = 0;
+let j = 0;
+let row = ''
+const size = process.argv.slice(2);
+
+if (size === undefined) {
+    console.log('Missing size');
+    return 0;
+}
+while (i < size) {
+    while (j < size) {
+        row += '#'
+        j += 1;
+    }
+    console.log(row)
+    i += 1
+}
