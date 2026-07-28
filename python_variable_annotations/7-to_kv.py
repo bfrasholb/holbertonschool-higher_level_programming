91e1a1bfe0e5c26f272cbdea29e45c86dbd74769
bfrasholb <13065@holbertonstudents.com>
2026-07-28 16:16:56 +1000

    My bad forgot to square

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple of a string and float or int"""
-    return (k, v)
+    return (k, v ** 2)
