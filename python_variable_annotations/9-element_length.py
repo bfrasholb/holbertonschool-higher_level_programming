d0d73778b1b1e2c2c296da25ee768bc6cfe04a82
bfrasholb <13065@holbertonstudents.com>
2026-07-29 10:21:28 +1000

    Done

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Annotate this Bullshit"""
+from typing import Sequence, List, Iterable, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """What the fuck man"""
+    return [(i, len(i)) for i in lst]
