3f31c7472b33db5a0e940f60acd5549258ed74de
bfrasholb <13065@holbertonstudents.com>
2026-09-01 13:55:23 +1000

    Fixed indentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,5 +3,5 @@
 
 
 def index_range(page: int, page_size: int) -> tuple:
-	"""Function that returns indices of page"""
+    """Function that returns indices of page"""
     return ((page - 1) * page_size, page * page_size)
