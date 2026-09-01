3611d7d65540675a7a086c328d52c54d43011b86
bfrasholb <13065@holbertonstudents.com>
2026-09-01 14:31:06 +1000

    No assertion message?

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -28,7 +28,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Class method to fetch the page results"""
         assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0, "Arguments must be integers greater than 0"
+        assert page > 0 and page_size > 0
         indices = index_range(page, page_size)
         return self.dataset()[indices[0] : indices[1]]
 
