afeb5fdd20fcb891c90bb46ae3196322eb51bf16
bfrasholb <13065@holbertonstudents.com>
2026-09-01 13:52:56 +1000

    Page index Function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,7 @@
-#!/usr/bin/node
+#!/usr/bin/env python3
+"""Paging Module"""
 
 
 def index_range(page: int, page_size: int) -> tuple:
+	"""Function that returns indices of page"""
     return ((page - 1) * page_size, page * page_size)
