e24d0124ea1ed2327a133d2689174eac4829e37d
bfrasholb <13065@holbertonstudents.com>
2026-09-01 14:46:10 +1000

    brace changes

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -34,4 +34,4 @@ class Server:
 
 def index_range(page: int, page_size: int) -> tuple:
     """Function that returns indices of page"""
-    return ((page - 1) * page_size, page * page_size)
+    return (page - 1) * page_size, page * page_size
