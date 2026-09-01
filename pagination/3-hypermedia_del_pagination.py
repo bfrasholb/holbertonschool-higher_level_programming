c1e78ada504a12a5a3c0a9f13c1c133a058ce430
bfrasholb <13065@holbertonstudents.com>
2026-09-01 17:00:28 +1000

    Incorrect Assertion

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -38,7 +38,7 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Gets hyper index and survives entry removal"""
-        assert isinstance(index, int) and index >= 0 and index <= len(self.indexed_dataset()) / page_size
+        assert isinstance(index, int) and index >= 0
         next_index = index + page_size
         data = [self.indexed_dataset()[i] for i in range(index, index + page_size) if i in self.indexed_dataset()]
         return {"index": index,
