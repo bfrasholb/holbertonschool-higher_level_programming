fbbae98118689e02234752e09d8e580b9b959fa3
bfrasholb <13065@holbertonstudents.com>
2026-09-01 14:30:09 +1000

    Formatting?

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -12,11 +12,11 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Class init"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
